152b5f06ea83c0b7524bfdd89894de8a12e98445
OlgaLugacheva <lugachevaolga@gmail.com>
2022-11-26 12:39:19 +0300

    Реализовал дз по переменным

--- src/ru/sky/Main.java
+++ src/ru/sky/Main.java
@@ -3,6 +3,8 @@ package ru.sky;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("Hello,  world!");
     }
+
 }
